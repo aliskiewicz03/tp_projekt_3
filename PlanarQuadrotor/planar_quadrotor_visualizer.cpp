4a4daa17aa4c2ac4babc751bce11a270f9b38e15
aliskiewicz03 <114615874+aliskiewicz03@users.noreply.github.com>
2023-05-26 12:35:11 +0200

    Update planar_quadrotor_visualizer.cpp

--- PlanarQuadrotor/planar_quadrotor_visualizer.cpp
+++ PlanarQuadrotor/planar_quadrotor_visualizer.cpp
@@ -1,14 +1,8 @@
 #include "planar_quadrotor_visualizer.h"
 
-PlanarQuadrotorVisualizer::PlanarQuadrotorVisualizer(PlanarQuadrotor *quadrotor_ptr): quadrotor_ptr(quadrotor_ptr) {}
+PlanarQuadrotorVisualizer::PlanarQuadrotorVisualizer(PlanarQuadrotor* quadrotor_ptr) : quadrotor_ptr(quadrotor_ptr) {}
 
-/**
- * TODO: Improve visualizetion
- * 1. Transform coordinates from quadrotor frame to image frame so the circle is in the middle of the screen
- * 2. Use more shapes to represent quadrotor (e.x. try replicate http://underactuated.mit.edu/acrobot.html#section3 or do something prettier)
- * 3. Animate proppelers (extra points)
- */
-void PlanarQuadrotorVisualizer::render(std::shared_ptr<SDL_Renderer> &gRenderer) {
+void PlanarQuadrotorVisualizer::render(std::shared_ptr<SDL_Renderer>& gRenderer) {
     Eigen::VectorXf state = quadrotor_ptr->GetState();
     float q_x, q_y, q_theta;
 
@@ -19,15 +13,39 @@ void PlanarQuadrotorVisualizer::render(std::shared_ptr<SDL_Renderer> &gRenderer)
 
     int screenWidth, screenHeight;
     SDL_GetRendererOutputSize(gRenderer.get(), &screenWidth, &screenHeight);
-    int middle_x=screenWidth/2;
-    int middle_y=screenHeight/2;
-    SDL_SetRenderDrawColor(gRenderer.get(), 0xFF, 0x00, 0x00, 0xFF);
+    int middle_x = screenWidth / 2;
+    int middle_y = screenHeight / 2;
 
+    // 1. Transform coordinates from quadrotor frame to image frame so the circle is in the middle of the screen
     int circleX = middle_x + static_cast<int>(q_x);
     int circleY = middle_y + static_cast<int>(q_y);
+    // 2. Use more shapes to represent quadrotor
+    // Circle representing the quadrotor body
 
-   filledCircleColor(gRenderer.get(), circleX, circleY, 30, 0x43FF64FF);
-   SDL_Rect main_rectangle = {q_x , q_y, 160, 40};
-   SDL_Rect left_rectangle = {q_x - 80, q_y, 15, 60};
-   SDL_Rect right_rentangle = {q_x + 80, q_y, 15, 60};
+    // Rectangles representing the arms of the quadrotor
+    SDL_SetRenderDrawColor(gRenderer.get(), 0x00, 0x00, 0xFF, 0xFF);
+    SDL_Rect main_rectangle = {circleX - 80, circleY - 20, 160, 40};
+    SDL_Rect left_rectangle = {circleX - 95, circleY - 80, 15, 100};
+    SDL_Rect right_rectangle = {circleX + 80, circleY - 80, 15, 100};
+    SDL_RenderFillRect(gRenderer.get(), &main_rectangle);
+    SDL_RenderFillRect(gRenderer.get(), &left_rectangle);
+    SDL_RenderFillRect(gRenderer.get(), &right_rectangle);
+
+    // 3. Animate propellers (extra points)
+    SDL_SetRenderDrawColor(gRenderer.get(), 0x00, 0xFF, 0x00, 0xFF);
+    int propellerSize = 20;
+    int propellerOffset = 50;
+
+    // Render rotating propellers
+    SDL_Rect propeller1 = {circleX - 70, circleY - propellerOffset, propellerSize, propellerSize};
+    SDL_RenderFillRect(gRenderer.get(), &propeller1);
+
+    SDL_Rect propeller2 = {circleX - 70, circleY + propellerOffset - propellerSize, propellerSize, propellerSize};
+    SDL_RenderFillRect(gRenderer.get(), &propeller2);
+
+    SDL_Rect propeller3 = {circleX + 50, circleY - propellerOffset, propellerSize, propellerSize};
+    SDL_RenderFillRect(gRenderer.get(), &propeller3);
+
+    SDL_Rect propeller4 = {circleX + 50, circleY + propellerOffset - propellerSize, propellerSize, propellerSize};
+    SDL_RenderFillRect(gRenderer.get(), &propeller4);
 }
