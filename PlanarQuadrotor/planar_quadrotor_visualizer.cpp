fba8250c57871337b707d87054c27d044916fb5c
DominikaK596 <126981951+DominikaK596@users.noreply.github.com>
2023-05-30 23:40:48 +0200

    Update planar_quadrotor_visualizer.cpp

--- PlanarQuadrotor/planar_quadrotor_visualizer.cpp
+++ PlanarQuadrotor/planar_quadrotor_visualizer.cpp
@@ -1,9 +1,21 @@
 #include "planar_quadrotor_visualizer.h"
+#include <cmath>
+#include <time.h>
+#include<random>
 
 PlanarQuadrotorVisualizer::PlanarQuadrotorVisualizer(PlanarQuadrotor* quadrotor_ptr) : quadrotor_ptr(quadrotor_ptr) {}
 
 void PlanarQuadrotorVisualizer::render(std::shared_ptr<SDL_Renderer>& gRenderer) {
     Eigen::VectorXf state = quadrotor_ptr->GetState();
+
+    std::uniform_real_distribution<float> y_distribution(-1.0f, 1.0f);
+
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_real_distribution<double> dist(-1.0, 1.0);
+
+    double randomValue = dist(gen);
+
     float q_x, q_y, q_theta;
 
     /* x, y, theta coordinates */
@@ -11,12 +23,14 @@ void PlanarQuadrotorVisualizer::render(std::shared_ptr<SDL_Renderer>& gRenderer)
     q_y = state[1];
     q_theta = state[2];
 
+
+
     int screenWidth, screenHeight;
     SDL_GetRendererOutputSize(gRenderer.get(), &screenWidth, &screenHeight);
     int middle_x = screenWidth / 2;
     int middle_y = screenHeight / 2;
     int circleX = middle_x + static_cast<int>(q_x);
-    int circleY = middle_y + static_cast<int>(q_y);
+    int circleY = middle_y + static_cast<int>(q_y+ randomValue);
 
     // main rectangle
     SDL_Rect main_rectangle = {circleX - 80, circleY - 20, 160, 40};
