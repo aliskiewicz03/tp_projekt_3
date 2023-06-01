263fe42bd2f27bbeb6609b662022c9bda37d85ec
aliskiewicz03 <114615874+aliskiewicz03@users.noreply.github.com>
2023-06-01 20:12:00 +0200

    Update simulate.cpp

--- PlanarQuadrotor/simulate.cpp
+++ PlanarQuadrotor/simulate.cpp
@@ -3,8 +3,7 @@
 #include <Eigen/Dense>
 #include <memory>
 #include <matplot/matplot.h>
-#include <random>
-#include <time.h>
+
 #include "planar_quadrotor.h"
 #include "planar_quadrotor_visualizer.h"
 
@@ -16,8 +15,8 @@ void plotTrajectory(const std::vector<float>& x_history, const std::vector<float
     matplot::show();
 }
 
-Eigen::MatrixXf LQR(PlanarQuadrotor& quadrotor, float dt) {
-    /* Calculate LQR gain matrix */
+Eigen::MatrixXf LQR(PlanarQuadrotor& quadrotor, float dt) 
+{
     Eigen::MatrixXf Eye = Eigen::MatrixXf::Identity(6, 6);
     Eigen::MatrixXf A = Eigen::MatrixXf::Zero(6, 6);
     Eigen::MatrixXf A_discrete = Eigen::MatrixXf::Zero(6, 6);
@@ -40,106 +39,96 @@ Eigen::MatrixXf LQR(PlanarQuadrotor& quadrotor, float dt) {
     return LQR(A_discrete, B_discrete, Q, R);
 }
 
-void control(PlanarQuadrotor& quadrotor, const Eigen::MatrixXf& K) {
+void control(PlanarQuadrotor& quadrotor, const Eigen::MatrixXf& K) 
+{
     Eigen::Vector2f input = quadrotor.GravityCompInput();
     quadrotor.SetInput(input - K * quadrotor.GetControlState());
 }
 
-int init(std::shared_ptr<SDL_Window>& gWindow, std::shared_ptr<SDL_Renderer>& gRenderer, const int SCREEN_WIDTH, const int SCREEN_HEIGHT) {
-    if (SDL_Init(SDL_INIT_VIDEO) >= 0) {
+int init(std::shared_ptr<SDL_Window>& gWindow, std::shared_ptr<SDL_Renderer>& gRenderer, const int SCREEN_WIDTH, const int SCREEN_HEIGHT) 
+{
+    if (SDL_Init(SDL_INIT_VIDEO) >= 0) 
+    {
         SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "1");
         gWindow = std::shared_ptr<SDL_Window>(SDL_CreateWindow("Planar Quadrotor", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN), SDL_DestroyWindow);
         gRenderer = std::shared_ptr<SDL_Renderer>(SDL_CreateRenderer(gWindow.get(), -1, SDL_RENDERER_ACCELERATED), SDL_DestroyRenderer);
         SDL_SetRenderDrawColor(gRenderer.get(), 0xFF, 0xFF, 0xFF, 0xFF);
-    } else {
+    } else 
+    {
         std::cout << "SDL_ERROR: " << SDL_GetError() << std::endl;
         return -1;
     }
     return 0;
 }
 
-int main(int argc, char* args[]) {
+int main(int argc, char* args[]) 
+{
     std::shared_ptr<SDL_Window> gWindow = nullptr;
     std::shared_ptr<SDL_Renderer> gRenderer = nullptr;
-    int screenWidth; 
-    int screenHeight;
-    SDL_GetRendererOutputSize(gRenderer.get(), &screenWidth, &screenHeight);
-
-    const int SCREEN_WIDTH = screenWidth;
-    const int SCREEN_HEIGHT = screenHeight;
+    const int SCREEN_WIDTH = 1280;
+    const int SCREEN_HEIGHT = 720;
 
     Eigen::VectorXf initial_state = Eigen::VectorXf::Zero(6);
-    int center_x=SCREEN_WIDTH/2, center_y=SCREEN_HEIGHT/2;
+    int middle_x=SCREEN_WIDTH/2, middle_y=SCREEN_HEIGHT/2;
+
     int beginning_x, beginning_y;
     srand(time(NULL));
     beginning_x=rand()%SCREEN_WIDTH;
     beginning_y=rand()%SCREEN_HEIGHT;
 
-    initial_state << beginning_x,beginning_y,0,0,0,0;
+    initial_state<< beginning_x,beginning_y,0,0,0,0;
     PlanarQuadrotor quadrotor(initial_state);
     PlanarQuadrotorVisualizer quadrotor_visualizer(&quadrotor);
 
-    int mouse_x = 0;
-    int mouse_y = 0;
 
     Eigen::VectorXf goal_state = Eigen::VectorXf::Zero(6);
-    goal_state <<  center_x, center_y, 0, 0, 0, 0;
+    goal_state <<  middle_x, middle_y, 0, 0, 0, 0;
     quadrotor.SetGoal(goal_state);
 
-    const float dt = 0.003;
+    const float dt = 0.001;
     Eigen::MatrixXf K = LQR(quadrotor, dt);
     Eigen::Vector2f input = Eigen::Vector2f::Zero(2);
 
     std::vector<float> x_history;
     std::vector<float> y_history;
     std::vector<float> theta_history;
-   
-
-   
-
-    if (init(gWindow, gRenderer, SCREEN_WIDTH, SCREEN_HEIGHT) >= 0) {
+  
+    if (init(gWindow, gRenderer, SCREEN_WIDTH, SCREEN_HEIGHT) >= 0) 
+    {
         SDL_Event e;
         bool quit = false;
         float delay;
         int x, y;
         Eigen::VectorXf state = Eigen::VectorXf::Zero(6);
        
-
-        while (!quit) {
-            while (SDL_PollEvent(&e) != 0) {
-                if (e.type == SDL_QUIT) {
+        while (!quit) 
+        {
+            while (SDL_PollEvent(&e) != 0) 
+            {
+                if (e.type == SDL_QUIT) 
                     quit = true;
-                } else if (e.type == SDL_MOUSEBUTTONDOWN && e.button.button == SDL_BUTTON_LEFT) {
+                else if (e.type == SDL_MOUSEBUTTONDOWN && e.button.button == SDL_BUTTON_LEFT) 
+                {
                     SDL_GetMouseState(&x, &y);
                     std::cout << "Mouse position: (" << x << ", " << y << ")" << std::endl;
-                    
-                    // Transform mouse coordinates to quadrotor world coordinates
-                    // Adjust the transformation according to your needs
                     float quad_x = static_cast<float>(x) / SCREEN_WIDTH;
                     float quad_y = static_cast<float>(y) / SCREEN_HEIGHT;
-
-                    // Set the goal state of the quadrotor
                     Eigen::VectorXf goal_state = Eigen::VectorXf::Zero(6);
                     goal_state << quad_x, quad_y, 0, 0, 0, 0;
                     quadrotor.SetGoal(goal_state);
-                } else if (e.type == SDL_KEYDOWN && e.key.keysym.sym == SDLK_p) {
-                    // Plot trajectory when 'p' key is pressed
+                } 
+                else if (e.type == SDL_KEYDOWN && e.key.keysym.sym == SDLK_p) 
                     plotTrajectory(x_history, y_history, theta_history);
-                }
+  
             }
 
             SDL_Delay(static_cast<int>(dt * 1000));
-
             SDL_SetRenderDrawColor(gRenderer.get(), 0xFF, 0xFF, 0xFF, 0xFF);
             SDL_RenderClear(gRenderer.get());
-
             quadrotor_visualizer.render(gRenderer);
             SDL_RenderPresent(gRenderer.get());
-
             control(quadrotor, K);
             quadrotor.Update(dt);
-
-            // Update trajectory history
             x_history.push_back(quadrotor.GetState()[0]);
             y_history.push_back(quadrotor.GetState()[1]);
             theta_history.push_back(quadrotor.GetState()[2]);
